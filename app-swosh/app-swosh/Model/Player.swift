ce49abfecb5aabe57b9aebd1c0faef4cb67a7f25
FullFrontal92 <adam.benyahia@hotmail.fr>
2018-02-15 18:00:22 +0100

    end of three screen app

---
+++ app-swosh/app-swosh/Model/Player.swift
@@ -0,0 +1,16 @@
+//
+//  Player.swift
+//  app-swosh
+//
+//  Created by Adam Benyahia on 14/02/2018.
+//  Copyright © 2018 Adam Benyahia. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+    
+}
